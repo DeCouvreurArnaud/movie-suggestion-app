d9f425a0d7a367c6df35183a321dc5d27de6cbbe
Arnaud De Couvreur student <arnaud.de.couvreur@student.howest.be>
2022-11-20 18:16:56 +0100

    get movie title from api

--- js/omdb.js
+++ js/omdb.js
@@ -1,4 +1,26 @@
 "use strict";
 
-const hello = "hello world"
-console.log(hello);
+const movieTitle = document.querySelector(".js-movie-title");
+const button = document.querySelector(".js-button");
+
+const apiKey = "96fab62e";
+const imdbMovies = ["tt3896198", "tt9114286", "tt4786824"];
+
+function getRandomMovieId() {
+  var movieId = imdbMovies[Math.floor(Math.random() * imdbMovies.length)];
+  console.log(movieId);
+  return movieId;
+}
+
+async function getMovie(imdbId) {
+  const url = `http://www.omdbapi.com/?i=${imdbId}&apikey=${apiKey}`;
+  const res = await fetch(`${url}`);
+  const data = await res.json();
+  console.log(data.Title);
+  movieTitle.innerHTML = data.Title;
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM fully loaded and parsed");
+  document.addEventListener("click", getMovie(getRandomMovieId()));
+});
